693928d49844fb29238b0831611cc92386e4ca6b
goodcodeguy <goodcodeguy@gmail.com>
2014-10-06 23:17:05 -0400

    added d3 api modules

--- lib/battlenet/api/d3_client.rb
+++ lib/battlenet/api/d3_client.rb
@@ -1,8 +1,14 @@
 require 'battlenet/api/client'
 
+require 'battlenet/modules/d3/profile'
+require 'battlenet/modules/d3/data'
+
 module Battlenet
 
 	class D3Client < Client
+
+		include Battlenet::Modules::D3::Profile
+		include Battlenet::Modules::D3::Data
 
 		def initialize(api_key = nil, region = :us)
 			super(api_key, :d3, region)
