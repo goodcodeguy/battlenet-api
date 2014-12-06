d988f82c10c4cf5adec33d30494bb2f8cc784493
goodcodeguy <goodcodeguy@gmail.com>
2014-12-05 19:48:05 -0500

    wrote tests to address #1

--- spec/battlenetapi_spec.rb
+++ spec/battlenetapi_spec.rb
@@ -1,11 +1,23 @@
 require 'battlenet/api'
 
 describe Battlenet::Client do
-  before(:all) do
+  it "should allow you to configure it for a valid region" do
     Battlenet.configure do |config|
       config.api_key = 'test-api'
-      config.region  = :us
+      config.region  = :eu
     end
+
+    c = Battlenet.WOWClient
+    expect(c.region).to eq(:eu)
+  end
+
+  it "should have the default region of :us" do
+    Battlenet.configure do |config|
+      config.api_key = 'test-api'
+    end
+
+    c = Battlenet.WOWClient
+    expect(c.region).to eq(:us)
   end
 
 end
