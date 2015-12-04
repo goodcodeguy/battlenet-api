81ef96e1e33bcba640f8f4b2415669d5027de75c
Nick Shepherd <goodcodeguy@gmail.com>
2015-12-04 13:26:47 -0500

    Add unit test to test for spaces in realm name

--- spec/battlenetapi_spec.rb
+++ spec/battlenetapi_spec.rb
@@ -1,21 +1,17 @@
 require 'battlenet/api'
 
 describe Battlenet::Client do
-  it "should allow you to configure it for a valid region" do
-    Battlenet.configure do |config|
-      config.api_key = 'test-api'
-      config.region  = :eu
+  context "when special characters are in realm names" do
+    before do
+      Battlenet.configure do |config|
+        config.api_key = 'test-api'
+        config.region  = :eu
+      end
     end
 
-    a = Battlenet.WOWClient
-    expect(a.region).to eq(:eu)
-
-    Battlenet.configure do |config|
-      config.region = :us
+    it "should escape spaces in realm names" do
+      c = Battlenet::Client.new({domain: 'http://www.test.com', endpoint: "/emerald dream/mal'ganis"})
+      expect(c.endpoint).to eq('/emerald%20dream/mal\'ganis')
     end
-
-    b = Battlenet.WOWClient
-    expect(b.region).to eq(:us)
-    
   end
 end
