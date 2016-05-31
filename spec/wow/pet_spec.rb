2d95651fcd4ff74eb5390cf2ce6a06ad056d0bf4
Robert Wachs <github@robert-wachs.de>
2016-05-31 21:01:46 +0200

    Fix WOW Pet API endpoint

--- spec/wow/pet_spec.rb
+++ spec/wow/pet_spec.rb
@@ -2,6 +2,25 @@ require 'spec_helper'
 
 describe Battlenet::WOW::Pet do
 
+  context "when looking up a species" do
+    before do
+      Battlenet.configure do |config|
+        config.api_key = ENV['BATTLENET_API_KEY']
+        config.region  = :us
+      end
+      @wow_client = Battlenet.WOWClient
+    end
+
+    it "should be able to fetch details" do
+      sid = 1134
+      VCR.use_cassette("pet/species/#{sid}") do
+        species = @wow_client.pet.species(sid)
+        expect(species['speciesId']).to eq(sid)
+        expect(species).to have_key('abilities')
+      end
+    end
+  end
+
   it { should respond_to(:master_list) }
   it { should respond_to(:ability) }
   it { should respond_to(:species) }
