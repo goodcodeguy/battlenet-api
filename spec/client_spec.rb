1b9708676e0f857766c27b76a0a65e8f6cc8e9fb
Andrej Dragojevic <andrejdr.93@gmail.com>
2018-02-26 18:53:28 +0100

    Add failing spec for overriding client base uri

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,13 +1,14 @@
 require 'spec_helper'
 
 describe Battlenet::Client do
+  it 'is expected not to override API url when creating multiple clients' do
+    eu_client = Battlenet::Client.new(api_key: 'api_key', region: :eu, endpoint: '/wow')
+    expect(eu_client.class.base_uri).to eq 'https://eu.api.battle.net/wow'
 
-  before do
-    Battlenet.configure do |config|
-      config.api_key = ENV['BATTLENET_API_KEY']
-      config.region  = :us
-    end
-    @client = Battlenet::Client.new
+    us_client = Battlenet::Client.new(api_key: 'api_key', region: :us, endpoint: '/wow')
+    expect(us_client.class.base_uri).to eq 'https://us.api.battle.net/wow'
+
+    expect(eu_client.class.base_uri).to eq 'https://eu.api.battle.net/wow'
   end
 
   # it "should translate foreign characters for URLS (whispä)" do
@@ -17,5 +18,4 @@ describe Battlenet::Client do
   # it "should translate spaces accurately for URLS (emerald dream)" do
   #   character = @client.character({:realm => 'emerald dream', :character_name => 'pftpft'})
   # end
-
 end
